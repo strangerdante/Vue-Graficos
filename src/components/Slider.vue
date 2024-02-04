8b09124145b4118adc4efbac76a3dd504d5175a1
Jamel <strangerdante@gmail.com>
2024-02-03 19:48:41 -0500

    proyecto inicial

---
+++ src/components/Slider.vue
@@ -0,0 +1,26 @@
+<template>
+  <swiper-container
+    class="rounded-xl"
+    navigation="true"
+    pagination="true"
+    scrollbar="true"
+  >
+    <swiper-slide
+      ><img src="https://picsum.photos/1200/400?random=1"
+    /></swiper-slide>
+    <swiper-slide
+      ><img src="https://picsum.photos/1200/400?random=2"
+    /></swiper-slide>
+    <swiper-slide
+      ><img src="https://picsum.photos/1200/400?random=3"
+    /></swiper-slide>
+  </swiper-container>
+</template>
+
+<script>
+import { register } from "swiper/element/bundle";
+// register Swiper custom elements
+register();
+</script>
+
+<style></style>
